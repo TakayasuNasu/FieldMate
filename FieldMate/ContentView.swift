cc3548cf60f1f63356c5d149ec8a35cd9903a607
takayasu.nasu <taka.beckham@gmail.com>
2023-07-27 20:13:28 -0700

    Add Home View

--- FieldMate/ContentView.swift
+++ FieldMate/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
   var body: some View {
     NavigationView {
       VStack {
+        NavigationLink(destination: Home()) {
+          Text("Go Home!")
+            .padding()
+        }
+
         NavigationLink(destination: SubView()) {
           Text("Go SubView!")
             .padding()
@@ -32,6 +37,94 @@ struct ContentView: View {
         }
       }
     }
+  }
+}
+
+struct Home: View {
+
+  @State var index = 0
+
+  var body: some View {
+
+    ZStack {
+
+      VStack {
+
+        Spacer()
+
+        HStack(spacing: 0) {
+
+          Button(action: {
+
+            self.index = 0
+
+          }) {
+            Image("home")
+              .resizable()
+              .frame(width: 25, height: 25)
+              .foregroundColor(self.index == 0 ? .white : Color.white.opacity(0.35))
+              .padding(.horizontal)
+          }
+
+          Spacer(minLength: 0)
+
+          Button(action: {
+
+            self.index = 1
+
+          }) {
+            Image("search")
+              .resizable()
+              .frame(width: 25, height: 25)
+              .foregroundColor(self.index == 1 ? .white : Color.white.opacity(0.35))
+              .padding(.horizontal)
+          }
+
+          Spacer(minLength: 0)
+
+          Button(action: {
+
+          }) {
+            Image("upload")
+              .renderingMode(.original)
+              .resizable()
+              .frame(width: 50, height: 35)
+              .padding(.horizontal)
+          }
+
+          Spacer(minLength: 0)
+
+          Button(action: {
+
+            self.index = 2
+
+          }) {
+            Image("comment")
+              .resizable()
+              .frame(width: 25, height: 25)
+              .foregroundColor(self.index == 2 ? .white : Color.white.opacity(0.35))
+              .padding(.horizontal)
+          }
+
+          Spacer(minLength: 0)
+
+          Button(action: {
+
+            self.index = 3
+
+          }) {
+            Image("profile")
+              .resizable()
+              .frame(width: 25, height: 25)
+              .foregroundColor(self.index == 3 ? .white : Color.white.opacity(0.35))
+              .padding(.horizontal)
+          }
+
+        }
+        .padding(.horizontal)
+      }
+    }
+    .background(Color.black.edgesIgnoringSafeArea(.all))
   }
 }
 
